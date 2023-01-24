8d68d0e721df887201f64bff281a099b5770a80f
Mousumi Malik <mousumimalik445@gmail.com>
2023-01-24 15:06:22 +0530

    indenting

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function About() {
   return (
     <>
-      <h1 className="text-6xl text-white">Github Finder!</h1>
-      <p className="mb-4 text-2xl font-light">
+      <h1 className="text-6xl text-white text-center mb-24">Github Finder!</h1>
+      <p className="mb-4 text-2xl font-light text-center">
         A React app to search GitHub profiles and see profile details. This
         project is part of the
         <a href="#"> React Front To Back.</a>{" "}
